310d042ee1eb2ae80d8a8fb1a408a75d30db83e1
shraddha-singh <shraddhasingh92001@gmail.com>
2023-09-04 22:02:15 +0530

    type casting

--- typecasting.c
+++ typecasting.c
@@ -1,3 +1,11 @@
 //typecasting is use for convert a data type in any other data type..
 
 #include<stdio.h>
+int main()
+{
+    int a = 100;
+    float b = 10.10;
+    printf("%f",(float)a);//typecasting int to ---> float
+
+    printf("%d",(int)b); // typecastign float to -----> int
+}
