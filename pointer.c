bf5dd81800a2f5e797b618db0fb86857be8c22bd
shraddha-singh <shraddhasingh92001@gmail.com>
2023-09-09 22:04:59 +0530

    pointer

---
+++ pointer.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    printf("example of pointer----\n");
+    int a = 10;
+    int* ptr = &a; // it store the address of a
+    printf("the values of a is --  %d\n", a);
+    printf("the address of a is -- %x\n",ptr); // %x is return the hexadecimal number
+    printf("the address of pointer ptr is -- %p \n",&ptr);// %p return address of pointer..
+    return 0;
+}
